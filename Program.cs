64b06cc3df5a1aed7f05bc9bc9e0df1986e44e63
cameorose <cameorose@users.noreply.github.com>
2017-07-13 11:44:00 +1000

    Refactored code to make efficient/easy to follow
    - created Calculate function
    - created Validate function
    - altered comments for cohesion

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-// Program for calculating Scrabble word scores
-// as per the rules given by Hooroo Academy.
-// Author: Cameo Langford
-// Created on: 04/07/2017
+/// Program for calculating Scrabble word scores
+/// as per the rules given by Hooroo Academy.
+/// Author: Cameo Langford
+/// Created on: 04/07/2017
+
 
 using System;
 using System.Collections.Generic;
@@ -9,82 +10,118 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ScrabbleConsole
 {
-    class ScrabbleCalc
+    public class Scrabble
     {
-        // The main function of the program for calculating Scrabble word scores works as follows:
-        //  - takes user input,
-        //  - converts to lowercase,
-        //  - checks against each letter values and adds it a total score.
-        //  - present the final total score to user
-        //  - asks if another word score is desired otherwise exit.
-        
+        string word;
+        int score;
+
+        public Scrabble(string word)
+        {
+            this.word = word;
+        }
+
+        /// <summary>
+        /// Validates user input to ensure only letters are entered
+        /// Advises user if characters other than letters are entered
+        /// </summary>
+        /// <param name="word"></param>
+        public void Validate(string word)
+        {
+            bool result = word.All(Char.IsLetter);
+            if (result == true)
+            {
+                this.Calculate(word);
+            }
+            else
+            {
+                Console.WriteLine("Please input letters only.\n");
+            }
+        }
+
+        /// <summary>
+        /// Calculates user's scrabble score
+        /// </summary>
+        /// <param name="word"></param>
+        public void Calculate(string word)
+        {
+            foreach (char letter in word)
+            {
+                switch (letter)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                    case 'l':
+                    case 'n':
+                    case 'r':
+                    case 's':
+                    case 't':
+                        score += 1;
+                        break;
+                    case 'd':
+                    case 'g':
+                        score += 2;
+                        break;
+                    case 'b':
+                    case 'c':
+                    case 'm':
+                    case 'p':
+                        score += 3;
+                        break;
+                    case 'f':
+                    case 'h':
+                    case 'v':
+                    case 'w':
+                    case 'y':
+                        score += 4;
+                        break;
+                    case 'k':
+                        score += 5;
+                        break;
+                    case 'j':
+                    case 'x':
+                        score += 8;
+                        break;
+                    case 'q':
+                    case 'z':
+                        score += 10;
+                        break;
+
+                }
+            }
+
+            Console.WriteLine("Your word is: " + word + "\nYour score is: " + score);
+        }
+
+        /// <summary>
+        /// Main function
+        /// -- Takes user input
+        /// -- Runs Validate function against user input
+        /// -- If valid input runs Calculate function
+        /// -- Outputs user scrabble score
+        /// -- Asks user if they want to calculate another score
+        /// -- Loops until user exits
+        /// </summary>
+        /// <param name="args"></param>
+
         static void Main(string[] args)
         {
-            string answer = "";
+            string answer;
             do
             {
                 Console.WriteLine("Welcome to the Scrabble Score Calculator! Please input your word:\n");
-                string userInput = Console.ReadLine().ToLower();
-                int scrabbleScore = 0;
-                
-                foreach (char letter in userInput)
-                {
-                    switch (letter)
-                    {
-                        case 'a':
-                        case 'e':
-                        case 'i':
-                        case 'o':
-                        case 'u':
-                        case 'l':
-                        case 'n':
-                        case 'r':
-                        case 's':
-                        case 't':
-                            scrabbleScore += 1;
-                            break;
-                        case 'd':
-                        case 'g':
-                            scrabbleScore += 2;
-                            break;
-                        case 'b':
-                        case 'c':
-                        case 'm':
-                        case 'p':
-                            scrabbleScore += 3;
-                            break;
-                        case 'f':
-                        case 'h':
-                        case 'v':
-                        case 'w':
-                        case 'y':
-                            scrabbleScore += 4;
-                            break;
-                        case 'k':
-                            scrabbleScore += 5;
-                            break;
-                        case 'j':
-                        case 'x':
-                            scrabbleScore += 8;
-                            break;
-                        case 'q':
-                        case 'z':
-                            scrabbleScore += 10;
-                            break;
-
-                    }
-                }
-                Console.WriteLine("\nYour word is: " + userInput);
-                Console.WriteLine("Your Scrabble score is: " + scrabbleScore);
+                string word = Console.ReadLine().ToString();
+                Scrabble scrabble = new Scrabble(word);
+                scrabble.Validate(word);
                 Console.WriteLine("\nWould you like to score another word? Y/N");
                 answer = Console.ReadLine();
-            } while (answer == "Y" || answer == "y");
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            }
+            while ((answer == "y" || answer == "Y") ? true : (answer == "n" || answer == "N") ? false : true);
         }
     }
 }
-
